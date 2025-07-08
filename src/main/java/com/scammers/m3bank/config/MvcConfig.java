4b8b9e2de4ca32f995c10a82de802cf364b6155d
oyboy <atom_box687@mail.ru>
2025-07-08 19:57:18 +0300

    Removed unnecessary class

--- src/main/java/com/scammers/m3bank/config/MvcConfig.java
+++ src/main/java/com/scammers/m3bank/config/MvcConfig.java
@@ -14,5 +14,6 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("redirect:/account");
         registry.addViewController("/registration").setViewName("registration");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/history").setViewName("transactions");
     }
 }
